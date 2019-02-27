1a7f7857b7c443673cc1347029089556fcb84a3c
Robert Zitello <Rasic@outlook.com>
2019-02-27 05:36:21 -0600

    Change text to fade in and out

--- slides.js
+++ slides.js
@@ -1,17 +1,15 @@
-
-alert(quotesArray[0]);
-
 var quoteIndex = 0;
 showQuotes();
 
 function showQuotes() {
-    
-    var textCaption = document.getElementsByClassName("textCaption")[0];
 
     quoteIndex++;
     if (quoteIndex > quotesArray.length) {quoteIndex = 1}    
+    
 
-    textCaption.innerHTML = quotesArray[quoteIndex-1];
+    $('#quoteDiv').animate({'opacity': 0}, 2000, function () {
+        $(this).text(quotesArray[quoteIndex-1]);
+    }).animate({'opacity': 1}, 2000);
 
     setTimeout(showQuotes, 5000); // Change image every 2 seconds
 }
