2331f2a5649489b906ab30ad945bb25c0534a3db
Robert Zitello <Rasic@outlook.com>
2019-02-26 20:57:08 -0600

    simplify js and add in seperate list js to hold array of quotes

--- slides.js
+++ slides.js
@@ -1,16 +1,17 @@
-var slideIndex = 0;
-showSlides();
 
-function showSlides() {
-var i;
-var slides = document.getElementsByClassName("textCaption");
-for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-}
-slideIndex++;
-if (slideIndex > slides.length) {slideIndex = 1}    
+alert(quotesArray[0]);
 
-slides[slideIndex-1].style.display = "block";  
+var quoteIndex = 0;
+showQuotes();
 
-setTimeout(showSlides, 1000); // Change image every 2 seconds
+function showQuotes() {
+    
+    var textCaption = document.getElementsByClassName("textCaption")[0];
+
+    quoteIndex++;
+    if (quoteIndex > quotesArray.length) {quoteIndex = 1}    
+
+    textCaption.innerHTML = quotesArray[quoteIndex-1];
+
+    setTimeout(showQuotes, 5000); // Change image every 2 seconds
 }
